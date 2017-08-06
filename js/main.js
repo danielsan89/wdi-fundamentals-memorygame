e118fe45c9b089c8837da6c826bb516145d6df54
Daniel Izq <danielsan@daniels-mbp.home>
2017-08-06 17:49:56 +0200

    created memory game

--- js/main.js
+++ js/main.js
@@ -21,24 +21,38 @@ var cards = [
 	}
 ];
 var cardsInPlay = [];
-var flipCard= function(cardId){	
+var flipCard= function(){	
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src',cards[cardId].cardImage)
 	console.log("User flipped "+cards[cardId].rank);
 	console.log(cards[cardId].suit);
 	console.log(cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
+	checkForMath();
+}
+var createBoard= function(){
+	
+	for (var i = 0; i < cards.length; i++) {		
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click',flipCard);		
+		document.getElementById('game_board').appendChild(cardElement);
+  }
 }
 var checkForMath = function(){
 	if (cardsInPlay.length === 2){		
 		if(cardsInPlay[0] === cardsInPlay[1]){
 			alert("You found a match!");
+	
 		}
 		else
 			alert("Sorry try again...");
+		
 	}	
 }
 
-flipCard(0);
-flipCard(3);
-checkForMath();
+createBoard();
+
 
 
